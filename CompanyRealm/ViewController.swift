f6a3d370df8deb6799919543adce844950240b9f
Salih Cakmak <slhckmk01@gmail.com>
2023-03-04 12:26:47 +0300

    Initial Commit

---
+++ CompanyRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanyRealm
+//
+//  Created by Salih on 4.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
